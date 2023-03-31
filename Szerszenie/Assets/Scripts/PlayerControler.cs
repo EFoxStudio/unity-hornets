4188f4ab42e77cb8378d8f2f4403315f061ef82a
ymon1098 <82119483+ymon1098@users.noreply.github.com>
2023-04-01 00:40:31 +0200

    animations player

--- Szerszenie/Assets/Scripts/PlayerControler.cs
+++ Szerszenie/Assets/Scripts/PlayerControler.cs
@@ -10,6 +10,7 @@ public class PlayerControler : MonoBehaviour
     public Rigidbody2D rb;
     public float speed;
     public float jumpForce;
+    public Animator animator;
     bool isGrounded = true;
     public Transform GroundCheck1;
     public LayerMask groundLayer;
@@ -22,11 +23,15 @@ public class PlayerControler : MonoBehaviour
 
     private void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        Vector2 movement = new Vector2(h*speed, rb.velocity.y);
+        float h = Input.GetAxisRaw("Horizontal") * speed;
+
+        animator.SetFloat("Speed", Mathf.Abs(h));
+
+        Vector2 movement = new Vector2(h, rb.velocity.y);
         rb.velocity = movement;
 
         isGrounded = Physics2D.OverlapCircle(GroundCheck1.position, 0.05f, groundLayer);
+        animator.SetBool("isJump", !isGrounded);
 
         if (Input.GetButtonDown("Jump"))
             if (isGrounded)
