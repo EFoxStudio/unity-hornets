a61ec265f083f70c0f701d19392270886f93b583
K1LLERPL <hubert.lech4@gmail.com>
2023-04-01 04:12:04 +0200

    fixedGrid

--- Szerszenie/Assets/Scripts/Invaders.cs
+++ Szerszenie/Assets/Scripts/Invaders.cs
@@ -12,12 +12,15 @@ public class Invaders : MonoBehaviour
     {
         for (int row = 0; row < rows; row++)
         {
-            Vector3 rowPosition = new Vector3(0.0f, row * 2.0f, 0.0f);
+            float width = 2.0f + (this.columns - 1);
+            float height = 2.0f * (this.rows - 1);
+            Vector2 centering = new Vector2(-width / 2, -height / 2);
+            Vector3 rowPosition = new Vector3(centering.x,centering.y + (row * 2.0f), 0.0f);
             for (int col = 0; col < columns; col++)
             {
                 Invader invader = Instantiate(this.prefabs[row], this.transform);
                 Vector3 position = rowPosition;
-                position.x += col * 1.0f;
+                position.x += col * 2.0f;
                 invader.transform.position = position;
             }
         }
